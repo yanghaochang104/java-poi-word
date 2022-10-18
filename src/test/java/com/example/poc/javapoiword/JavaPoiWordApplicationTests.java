0b6c3ca590593426b1b49f6a0e72f95f99081d63
ray.chang <yanghaochang1004@gmail.com>
2022-10-18 16:06:45 +0800

    chore(init): project init

---
+++ src/test/java/com/example/poc/javapoiword/JavaPoiWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc.javapoiword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPoiWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
